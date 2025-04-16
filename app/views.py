17394f1858480678c8d13bd104d9f2f486045035
GabrielRibeiroRodrigues <gabriel.ribeiro@alunos.ifsuldeminas.edu.br>
2025-04-15 21:37:18 -0300

    revert de commit

--- app/views.py
+++ app/views.py
@@ -5,11 +5,9 @@ from django.views.generic import *
 from .models import *
 
 def home(request):
-    services = Service.objects.all()
-    portfolio_items = Portfolio.objects.all()
-    return render(request, 'base.html', {
-        'services': services,
-        'portfolio_items': portfolio_items
+  
+    return render(request, 'home.html', {
+        
     })
 
 
